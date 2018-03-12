e57dc33f0f4de00731d91569bd6b64225c8e80ff
bsj0810 <bsj0810@163.com>
2018-03-12 19:14:39 +0800

    test

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,55 +26,56 @@
         </div>
         <div class="media">
             <div class="m_box">
-                <form action="{!! route('register') !!}"  method="post" outsubmit="return false" autocomplete="off" class="rel">
-                    {!! csrf_field() !!}
+                <form action="{{ route('register') }}"  outsubmit="return false" autocomplete="off" class="rel" method="POST">
+                    {{ csrf_field() }}
                     <div class="sec">
-                        <div class="sec">
-                            <div class="sjh">
-                                <div class="s_rel">
-                                    <input type="tel" maxlength="11" class="mob" name="name" placeholder="用户名" data-error="请输入用户名" data-error-empty="请输入用户名"required>
-                                </div>
-                            </div>
-                            <div class="sjh">
-                                <div class="s_rel">
-                                    <input type="tel" maxlength="11" class="mob" name="phone" placeholder="手机号" data-type="phone" data-error="请输入正确的手机号" data-error-empty="请输入手机号"required>
-                                </div>
-                            </div>
-                             <div class="yzm">
-                                 <span class="dib">
-                                     <input type="tel"  class="verify" name="verify" placeholder="请输入图形验证码" data-type="imagecode" data-error="请输入4位图形验证码" data-error-empty="请输入图形验证码"required>
-                                 </span>
-                                 <label class="p_yzm">
-                                     {!! captcha_img() !!}
-                                 </label>
-                             </div>
-                            {{--<div class="yzm">
-                                <span class="dib">
-                                    <input type="tel" maxlength="4" class="verify" name="verify" placeholder="请输入短信验证码" data-type="imagecode" data-error="请输入短信验证码" data-error-empty="请输入短信验证码"required>
-                                </span>
-                                <label class="verify_btn">
-                                    <em class="send">发送验证码</em>
-                                    <em class="resend">重新发送验证码</em>
-                                </label>
-                            </div>--}}
-                            <div class="sjh">
-                                <div class="s_rel">
-                                    <input type="text" class="mm" name="password" placeholder="您的密码" data-type="password" data-error="您的密码" data-error-empty="您的密码" required>
-                                </div>
-                            </div>
-                            <div class="sjh">
-                                <div class="s_rel">
-                                    <input type="text" class="qmm" name="password_confirmation" placeholder="确认密码" data-type="password" data-error="确认密码" data-error-empty="确认密码" required>
-                                </div>
+                        <div class="sjh">
+                            <div class="s_rel">
+                                <input type="tel" maxlength="11" class="mob" name="name" placeholder="您的用户名" data-type="phone" data-error="请输入正确的用户名" data-error-empty="请输入用户名"required>
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
-                        <div class="btn-row">
-                            <button type="submit" class="qdl">
-                                <em class="sub-txt">注册</em>
-                            </button>
+                        <div class="yzm">
+                           <span class="dib">
+                               <input type="tel" maxlength="5" class="verify" name="captcha" placeholder="请输入图形验证码" data-type="imagecode" data-error="请输入4位图形验证码" data-error-empty="请输入图形验证码"required>
+                               @if ($errors->has('captcha'))
+                                   <span class="help-block">
+                                       <strong>{{ $errors->first('captcha') }}</strong>
+                                    </span>
+                               @endif
+                           </span>
+                            <label class="p_yzm">
+                                <img src="{{captcha_src()}}" onclick="this.src='/captcha/default?'+Math.random()" alt="验证码">
+                            </label>
+                        </div>
+
+                        <div class="sjh">
+                            <div class="s_rel">
+                                <input type="password" class="mm" name="password" placeholder="您的密码" data-type="password" data-error="您的密码" data-error-empty="您的密码"required>
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="sjh">
+                            <div class="s_rel">
+                                <input type="password" class="qmm" name="password_confirmation" placeholder="确认密码" data-type="password" data-error="确认密码" data-error-empty="确认密码"required>
+                            </div>
                         </div>
                     </div>
+                    <div class="btn-row">
+                        <button type="submit" class="qdl">
+                            <em class="sub-txt">注册</em>
+                        </button>
+                    </div>
                 </form>
+
             </div>
         </div>
     </div>
