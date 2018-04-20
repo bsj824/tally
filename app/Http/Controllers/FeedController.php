ba1fa4ce5ea74847869d8940129076ebf90bbaad
bsj0810 <bsj0810@163.com>
2018-04-20 22:00:26 +0800

    xiugai1

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\FeedbackRequest;
-use App\Models\feedback;
+use App\Models\Feedback;
 use Illuminate\Http\Request;
 
 class FeedController extends Controller
